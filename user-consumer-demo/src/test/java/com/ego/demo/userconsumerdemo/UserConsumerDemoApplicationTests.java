95a94b93889d536e6ff6937455c410074ffc01ec
15882574415 <1439286263@qq.com>
2020-05-19 19:19:17 +0800

    test2

---
+++ user-consumer-demo/src/test/java/com/ego/demo/userconsumerdemo/UserConsumerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ego.demo.userconsumerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserConsumerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
